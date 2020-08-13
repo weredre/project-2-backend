46f30fd38e1d056b957d1c33085b76a389e3daa4
Andre Heslop <andreheslop@gmail.com>
2020-08-13 17:26:44 -0400

    updated shadowamps seed

--- seed/shadowAmps.js
+++ seed/shadowAmps.js
@@ -5,5 +5,14 @@ const shadowAmpsSeed = [
   {name: "Lightening Bolt",damage:6},
   {name:"Acid Spit", damage:7}
 ]
+shadowAmps.insertMany(shadowAmpsSeed,
+  (error, response) => {
+      if (error) {
+          console.log(error);
+      } else {
+          console.log(response);
+      }
+      db.close()
+    })
 
 module.exports = shadowAmps
