6dbf4629f26ea847561e8be24c519a91a9844cce
Andre Heslop <andreheslop@gmail.com>
2020-08-13 10:07:08 -0400

    changed server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const characterRouter = require("./routes/character.js")
 // Variables with global scope
 const PORT = process.env.PORT; //port number for server as defined in environment variables
 const NODE_ENV = process.env.NODE_ENV; //"development" or "production"
-const mongoURI = process.env.mongoURI + "character"; //URI for connecting to database specified in .env
+const mongoURI = process.env.mongoURI; //URI for connecting to database specified in .env
 const db = mongoose.connection; //the mongoose connection object
 const mongoConfigObject = { useNewUrlParser: true, useUnifiedTopology: true }; //Config option to eliminate deprecation warnings
 
